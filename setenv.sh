8d34fd50ba9a3dc1df664ab4b6b488f7212b249e
Lester Claudio <claudiol@fedora.redhat.local>
2020-03-22 10:51:10 -0600

    Added env

--- setenv.sh
+++ setenv.sh
@@ -3,8 +3,7 @@
 export NDDSHOME=/opt/rti_connext_dds-6.0.1
 export CPLUS_INCLUDE_PATH=/usr/include/
 export LD_LIBRARY_PATH=/opt/rti_connext_dds-6.0.1/lib/x64Linux4gcc7.3.0
-export CPLUS_INCLUDE_PATH=/opt/rti_connext_dds-6.0.1/include/:/opt/rti_connext_dds-6.0.1/include/ndds:/opt/rti_connext_dds-6.0.1/include/recordingservice:/opt/rti_connext_dds-6.0.1/include/routeingservice:/opt/rti_connext_dds-6.0.1/include/rti:/opt/rti_connext_dds-6.0.1/include/rti_dl:/usr/include:/usr/include/std:/usr/include/c++/8/
+export CPLUS_INCLUDE_PATH=/opt/rti_connext_dds-6.0.1/include/:/opt/rti_connext_dds-6.0.1/include/ndds:/opt/rti_connext_dds-6.0.1/include/recordingservice:/opt/rti_connext_dds-6.0.1/include/routeingservice:/opt/rti_connext_dds-6.0.1/include/rti:/opt/rti_connext_dds-6.0.1/include/rti_dl:/usr/include:/usr/include/std:/usr/include/c++/8/:/opt/rh/devtoolset-4/root/usr/lib/gcc/x86_64-redhat-linux/5.3.1/
+ENV PATH $PATH:$NDDSHOME/bin:/opt/rh/devtoolset-4/root/usr/bin
 
 
-
-echo /opt/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.bash 
