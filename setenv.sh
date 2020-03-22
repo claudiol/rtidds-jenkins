606f817aa25a37f24a2aa6b4c44c3f45d0156c7d
Lester Claudio <claudiol@fedora.redhat.local>
2020-03-22 10:57:12 -0600

    added perm x

--- setenv.sh
+++ setenv.sh
@@ -4,6 +4,6 @@ export NDDSHOME=/opt/rti_connext_dds-6.0.1
 export CPLUS_INCLUDE_PATH=/usr/include/
 export LD_LIBRARY_PATH=/opt/rti_connext_dds-6.0.1/lib/x64Linux4gcc7.3.0
 export CPLUS_INCLUDE_PATH=/opt/rti_connext_dds-6.0.1/include/:/opt/rti_connext_dds-6.0.1/include/ndds:/opt/rti_connext_dds-6.0.1/include/recordingservice:/opt/rti_connext_dds-6.0.1/include/routeingservice:/opt/rti_connext_dds-6.0.1/include/rti:/opt/rti_connext_dds-6.0.1/include/rti_dl:/usr/include:/usr/include/std:/usr/include/c++/8/:/opt/rh/devtoolset-4/root/usr/lib/gcc/x86_64-redhat-linux/5.3.1/
-ENV PATH $PATH:$NDDSHOME/bin:/opt/rh/devtoolset-4/root/usr/bin
+export PATH=$PATH:$NDDSHOME/bin:/opt/rh/devtoolset-4/root/usr/bin
 
 
